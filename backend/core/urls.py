da4658eeed09d8dc8d160354c396a702a7b0b8a7
Rifqi Firdausi Arafadh <rfa354@ums.ac.id>
2026-07-17 12:20:14 +0700

    Serve uploaded media when production opt-in is enabled

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as serve_static_file
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.throttling import ScopedRateThrottle
@@ -19,5 +20,12 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-if settings.DEBUG or getattr(settings, 'SERVE_MEDIA', False):
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif getattr(settings, 'SERVE_MEDIA', False):
+    # django.conf.urls.static.static() intentionally returns no routes when
+    # DEBUG=False. This deployment explicitly opts into serving uploaded media
+    # from Django until a dedicated object store/web server is configured.
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve_static_file, {'document_root': settings.MEDIA_ROOT}),
+    ]
